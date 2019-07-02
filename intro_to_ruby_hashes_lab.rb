6bc9b90e786bc711c12a22d584ae8c92b96ccacb
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-02 21:15:07 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
-				:four_pieces_owned => 200,
+				:four_pieces_owned => 200
 			},
 			:names =>
 			{
@@ -63,24 +63,26 @@ def monopoly_with_fourth_tier
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
-				:four_pieces_owned => 200,
+				:four_pieces_owned => 200
 			},
 
 					:names =>
-								{
-									:reading_railroad => {
-					                  100
-									},
-									:pennsylvania_railroad => {
-					                  200
-									},
-									:b_and_o_railroad => {
-					                  400
-									},
-									:shortline => {
-					                  800
-					                  }
-
+					{
+						:reading_railroad=>
+						{
+							"mortgage_value"=>"$100"
+						},
+						:pennsylvania_railroad=>
+						{
+							"mortgage_value"=>"$200"
+						},
+						:b_and_o_railroad=>
+						{
+							"mortgage_value"=>"$400"
+						},
+						:shortline=>
+						{
+							"mortgage_value"=>"$800"
         } # closing
 			} # closing rent_in_dollars
 	  } # closing railroads hash
